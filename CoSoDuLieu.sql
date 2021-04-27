810eb6df33362b443b65b595d29f8d7f31b07d41
Dat Nguyen Thanh <datnguyennguyen.2000@gmail.com>
2021-04-27 20:09:06 +0700

    Lan2

--- CoSoDuLieu.sql
+++ CoSoDuLieu.sql
@@ -9,7 +9,8 @@ GO
 USE QLySinhVien
 
 CREATE TABLE SinhVien(
-	MaSinhVien VARCHAR(15) PRIMARY KEY,
+	IDSinhVien INT IDENTITY(1,1) PRIMARY KEY,
+	MaSinhVien VARCHAR(15) UNIQUE NOT NULL,
 	HoTen NVARCHAR(100) NULL,
 	MatKhau NVARCHAR(20) NULL
 	)
@@ -23,12 +24,12 @@ CREATE TABLE LopSinhHoat(
 GO
 
 CREATE TABLE ChiTietSinhVien(
-	MaSinhVien VARCHAR(15) FOREIGN KEY (MaSinhVien) REFERENCES dbo.SinhVien(MaSinhVien) NOT NULL,
+	IDSinhVien INT FOREIGN KEY (IDSinhVien) REFERENCES dbo.SinhVien(IDSinhVien) NOT NULL,
 	MaLSH INT FOREIGN KEY (MaLSH) REFERENCES dbo.LopSinhHoat(MaLSH) NOT NULL,
 	SoDienThoai VARCHAR(15) NULL,
 	GioiTinh BIT DEFAULT 1 NOT NULL,
 	Email VARCHAR(200) NULL
-	PRIMARY KEY(MaSinhVien)
+	PRIMARY KEY(IDSinhVien)
 )
 GO
 INSERT INTO dbo.SinhVien(MaSinhVien, HoTen, MatKhau)
@@ -60,12 +61,12 @@ VALUES
 	(1004, '18HTP3')
 
 
-INSERT INTO dbo.ChiTietSinhVien(MaSinhVien, MaLSH, GioiTinh, Email)
+INSERT INTO dbo.ChiTietSinhVien(IDSinhVien, MaLSH, GioiTinh, Email)
 VALUES
-('1811505310101',1,1,'1811505310101@gmail.com'),
-('1811505310102',1,0,'1811505310102@gmail.com'),
-('1811505310103',1,0,'1811505310103@gmail.com'),
-('1811505310104',1,0,'1811505310104@gmail.com'),
-('1811505310105',1,0,'1811505310105@gmail.com'),
-('1811505310106',1,1,'1811505310106@gmail.com'),
-('1811505310107',1,1,'1811505310107@gmail.com')
+(1,1,1,'1811505310101@gmail.com'),
+(2,1,0,'1811505310102@gmail.com'),
+(3,1,0,'1811505310103@gmail.com'),
+(4,1,0,'1811505310104@gmail.com'),
+(5,1,0,'1811505310105@gmail.com'),
+(6,1,1,'1811505310106@gmail.com'),
+(7,1,1,'1811505310107@gmail.com')
